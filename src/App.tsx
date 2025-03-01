2e5f1f7b0cf164b5430b0e1df9256522392169ae
Arman D <adrismir@gmail.com>
2025-03-01 15:17:16 -0800

    skeleton for fetching rfid

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Scan from "./pages/Scan";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
+import SeeRfid from "./pages/SeeRfid";
 import { PageTransition } from "./components/Layout";
 import { AnimatePresence } from "framer-motion";
 
@@ -27,6 +28,7 @@ const App = () => (
               <Route path="/scan" element={<Scan />} />
               <Route path="/auth" element={<Auth />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/see-rfid" element={<SeeRfid/>} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </PageTransition>
